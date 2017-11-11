e65cd49b68ea6a8a44049e12528fdc679d5329db
somesortoferror <somesortoferror@users.noreply.github.com>
2017-11-11 01:11:17 +0000

    handle paginated results

--- ebayautofb - rel.py
+++ ebayautofb - rel.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 import os
 import stat
 import random
 from ebaysdk.trading import Connection as Trading
 
 debug = False
+# in test mode, no actual feedback will be left
+test_mode = False
 
 # The following 4 variables MUST be set appropriately for this script to work
 # Get your eBay SDK keys at https://developer.ebay.com/
@@ -23,14 +27,13 @@ feedback_text=(
                 "Recommended eBayer, thank you",
                 "Good transaction, thank you"
               )
-              
-try:    
-    print("Fetching items awaiting feedback...")
-    api = Trading(appid=_appid, devid=_devid, certid=_certid, token=user_token, config_file=None)
-    response = api.execute('GetItemsAwaitingFeedback', {})
+
+def get_ebay_items(api,page_no=1):
     if debug:
-        print(response.dict())
-    print("Server response: {}".format(response.dict()['Ack']))
+        print("get_ebay_items: page {}".format(page_no))
+    return api.execute('GetItemsAwaitingFeedback', {'Pagination': {'PageNumber': page_no,'EntriesPerPage': 200}})
+
+def leave_feedback(api,response):
     i = 0
     for c,transaction in enumerate(response.dict()['ItemsAwaitingFeedback']['TransactionArray']['Transaction']):
         buyer_id=transaction['Buyer']['UserID']
@@ -41,11 +44,40 @@ try:
             fb_received=transaction['FeedbackReceived']['CommentType']
         except Exception as e:
             fb_received=""
+        if debug:
+            print("Transaction: Item {}, Buyer: {}, FB: {}, ID: {}".format(item_title,buyer_id,fb_received if fb_received != "" else "n/a",orderlineitem_id))
         if fb_received == "Positive":
-            response = api.execute('LeaveFeedback', {'OrderLineItemID': orderlineitem_id,'CommentType': 'Positive','CommentText':random.choice(feedback_text)})
-            print("Left feedback for user: {}, order_id: {}, item: {}".format(buyer_id,orderlineitem_id,item_title))
+            if test_mode:
+                print("Test mode: Feedback would be left for user {}, item {}".format(buyer_id,item_title))
+            else:
+                response = api.execute('LeaveFeedback', {'OrderLineItemID': orderlineitem_id,'CommentType': 'Positive','CommentText':random.choice(feedback_text)})
+                print("Left feedback for user: {}, order_id: {}, item: {}".format(buyer_id,orderlineitem_id,item_title))
             i+=1
-    print("Feedback left for {} transactions out of {}".format(i,c))
+    return i,c+1
+                  
+try:
+    print("Fetching items awaiting feedback...")
+    api = Trading(appid=_appid, devid=_devid, certid=_certid, token=user_token, config_file=None)
+    response = get_ebay_items(api)
+    if debug:
+        print(response.dict())
+    print("Server response: {}".format(response.dict()['Ack']))
+
+    result_pages=int(response.dict()['ItemsAwaitingFeedback']['PaginationResult']['TotalNumberOfPages'])
+    result_entries=int(response.dict()['ItemsAwaitingFeedback']['PaginationResult']['TotalNumberOfEntries'])
+    print("Entries: {}, Pages: {}".format(result_entries,result_pages))
+
+    def do_fb(a,r,page):
+        i,c = leave_feedback(a,r)
+        print("Page {}: Feedback left for {} transactions out of {}".format(page,i,c))
+    
+    # loop through each page, leaving feedback as we go
+    # todo: would be better to build complete list of items first, then leave fb
+    do_fb(api,response,1)
+    for page in range(2,result_pages+1):
+        response=get_ebay_items(api,page)
+        do_fb(api,response,page)
+        
 except ConnectionError as e:
     print(e)
     print(e.response.dict())
